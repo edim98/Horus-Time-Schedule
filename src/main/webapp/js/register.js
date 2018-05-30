ec6678e20b20d0101ff19d70194c493ae14f8a91
EduardCon <e.constantinescu@student.utwente.nl>
2018-05-30 17:28:11 +0200

    rework login and register

--- src/main/webapp/js/register.js
+++ src/main/webapp/js/register.js
@@ -2,37 +2,48 @@ $(document).ready(function() {
   $('#register-form').on("submit", function(event) {
     event.preventDefault();
 
-    var formData = {
-      'username' : $('#username').val(), // this can be the teacherID for now
-      'name' : 'Harry Arts',
-      'phone' : '0742069101',
-      'email' : $('#email').val(),
-      'password' : $('#password').val()
-    };
+    if($('#check').is(':checked')) {
+      var formData = {
+        'username' : $('#username').val(), // this can be the teacherID for now
+        'name' : 'Harry Arts',
+        'phone' : '0742069101',
+        'email' : $('#email').val(),
+        'password' : $('#password').val()
+      };
 
-    $.ajax({
-      url: '/horus/requests/register',
-      type: 'POST',
-      dataType: 'json',
-      data: formData
-      // success: function(result){
-      //   if(result.tatus == ok){
-      //     window.location.href = "./dashboard.html";
-      //   } else{
-      //     alert("Invalid data!");
-      //     location.reload();
-      //   }
-      // }
-    })
-    .done(function() {
-      console.log("success");
-    })
-    .fail(function() {
-      console.log("error");
-    })
-    .always(function() {
-      console.log("complete");
-    });
+      $.ajax({
+        url: '/horus/requests/register',
+        type: 'POST',
+        dataType: 'json',
+        data: formData
+        // success: function(result){
+        //   if(result.tatus == ok){
+        //     window.location.href = "./dashboard.html";
+        //   } else{
+        //     alert("Invalid data!");
+        //     location.reload();
+        //   }
+        // }
+      })
+      .done(function() {
+        console.log("success");
+        var url = "./dashboard.html";
+        $(location).attr('href', url);
+      })
+      .fail(function() {
+        console.log("error");
+        //location.reload();
+      })
+      .always(function() {
+        console.log("complete");
+      });
+    } else {
+      alert('Please agree with the terms and conditions!');
+    }
+  });
 
+  $('#sign-in').click(function(){
+    url = './login.html';
+    $(location).attr('href', url);
   });
 });
