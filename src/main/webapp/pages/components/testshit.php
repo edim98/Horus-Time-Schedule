a67c1bef7b74c2ae058c5a02328509f0da1754be
Rus Catalin <a.c.rus@student.utwente.nl>
2018-05-31 00:58:46 +0200

    Request form done. Needs two more tweaks (See TODOs)

--- src/main/webapp/pages/components/testshit.php
+++ src/main/webapp/pages/components/testshit.php
@@ -1,9 +1,1 @@
-<link href="../../js/jquery.datetimepicker.min.css" rel="stylesheet"/>
-<script src="../../js/jquery.js"></script>
-<script src="../../js/jquery.datetimepicker.full.js"></script>
-
-<input id="datetime">
-
-<script>
-  $("#datetime-input").datetimepicker();
-</script>
+<!-- EMPTY NOW.... USE THIS FOR TESTING -->
