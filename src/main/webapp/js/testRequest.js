63548c8cd7b99641e399dc96cc2205e0f49713af
EduardCon <e.constantinescu@student.utwente.nl>
2018-05-31 11:12:52 +0200

    changes

---
+++ src/main/webapp/js/testRequest.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $('.test').click(function(){
+
+    var request = JSON.stringify({
+      'oldRoom' : 'CI T100',
+      'newRoom' : 'RA 1501',
+      'oldDate' : '69aprilie',
+      'newDate' : '31 februarie',
+      'teacherId' : 'm1234567',
+      'numberOfStudents' : '200',
+      'requestType' : 'reschedule'
+    });
+
+    $.ajax({
+      url: '/horus/requests',
+      type: 'POST',
+      dataType: 'json',
+      data: request,
+      success : function(data){
+          alert('OK!');
+      },
+      complete : function(data) {
+        console.log('Am trimis!');
+      }
+    });
+  });
+});
