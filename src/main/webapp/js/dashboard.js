874fd8e5e30933fc57c3a82b81fac88fb2c9fd8c
Adrian Pop <a.pop@student.utwente.nl>
2018-06-15 18:02:09 +0200

    dashboard javascript

---
+++ src/main/webapp/js/dashboard.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $.ajax({
+    url: 'horus/requests/pending',
+    type: 'GET',
+    dataType: 'json',
+    headers: {
+      'Accept' : 'application/json',
+      'Content-Type' : 'application/json'
+    }
+  })
+  .done(function(result) {
+    alert("success");
+    document.getElementById("pendingRequests").innerHTML(result);
+  })
+  .fail(function(result) {
+    alert("error");
+    console.log("error did not get requests");
+    console.log(result);
+  })
+  .always(function() {
+    console.log("complete");
+  });
+
+});
