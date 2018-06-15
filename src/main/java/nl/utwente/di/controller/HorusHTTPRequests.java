576653cdd162c03275959a84895324c0dd0ce585
Andrei <a.popa@student.utwente.nl>
2018-06-15 17:40:08 +0200

    kasdgfh

--- src/main/java/nl/utwente/di/controller/HorusHTTPRequests.java
+++ src/main/java/nl/utwente/di/controller/HorusHTTPRequests.java
@@ -4,6 +4,7 @@ import nl.utwente.di.exceptions.InvalidInputException;
 import nl.utwente.di.model.Lecturer;
 import nl.utwente.di.model.Request;
 import nl.utwente.di.model.Room;
+import nl.utwente.di.model.Status;
 import org.json.JSONObject;
 
 import javax.ws.rs.*;
@@ -96,5 +97,16 @@ public class HorusHTTPRequests {
     @Produces("application/json")
     public int getPendingRequests() {
         return DatabaseCommunication.getPendingRequests();
+    }
+
+    @PUT
+    @Path("/statuschange")
+    @Consumes("application/json")
+    public Response changeStatus(String jsonBody) {
+        JSONObject jsonObject = new JSONObject(jsonBody);
+        String status = jsonObject.getString("status");
+        int id = jsonObject.getInt("id");
+        DatabaseCommunication.changeRequestStatus(Status.valueOf(status), id);
+        return Response.status(Response.Status.OK).build();
     }
 }
