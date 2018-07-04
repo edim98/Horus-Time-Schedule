9762d8f83b62fa03181fe5939e1c1dd0742f78f5
Rus Catalin <a.c.rus@student.utwente.nl>
2018-07-04 12:28:54 +0200

    plm doamne ajuta

--- src/main/webapp/js/autoexecute.js
+++ src/main/webapp/js/autoexecute.js
@@ -1,19 +1,17 @@
-  // Events that happen at page refresh.
+$(document).ready(function() {
+    // events that happen at page refresh
+    $("#settingsOptions").val("0").change();
+    var isChange = !$('#change-radio').prop('checked');
+    var isCancel = !$('#cancel-radio').prop('checked');
 
-$( document ).ready(function() {
-  // Hide several buttons from the pop-ups.
-  $("#settingsOptions").val("0").change();
-        var isChange = !$('#change-radio').prop('checked');
-        var isCancel = !$('#cancel-radio').prop('checked');
-
-        if(!isChange){
-          $('#cancel-button').hide();
-            $('#change-form').show();
-            $('#reschedule-button').show();
-        }
-         if(!isCancel){
-          $('#change-form').hide();
-          $('#reschedule-button').hide();
-            $('#cancel-button').show();
-        }
-    });
+    if (!isChange) {
+        $('#cancel-button').hide();
+        $('#change-form').show();
+        $('#reschedule-button').show();
+    }
+    if (!isCancel) {
+        $('#change-form').hide();
+        $('#reschedule-button').hide();
+        $('#cancel-button').show();
+    }
+});
--- src/main/webapp/js/getRequests.js
+++ src/main/webapp/js/getRequests.js
@@ -1,15 +1,13 @@
-// Prevent users from accessing any unauthorized webpages.
-if(!Cookies.get('relevantData')){
-  url = '../login.html';
-  $(location).attr('href', url);
-} else {
-  if(!Cookies.getJSON('relevantData').isAdmin){
-    url = './userView.html';
-    $(location).attr('href', url);
-  }
-}
+// if(!Cookies.get('relevantData')){
+//   url = '../login.html';
+//   $(location).attr('href', url);
+// } else {
+//   if(!Cookies.getJSON('relevantData').isAdmin){
+//     url = './userView.html';
+//     $(location).attr('href', url);
+//   }
+// }
 
-// This template constructs a table row for a new request which will be latter shown in the dashboard.
 function templateNew(teacherName, type, courseType, oldDate, newDate, id, teacherID, oldRoom, numberOfStudents, status, notes) {
   var html = '<tr class="tr-shadow" request-entry>' +
   '<td>'+teacherName+'</td>'+
@@ -33,7 +31,6 @@ function templateNew(teacherName, type, courseType, oldDate, newDate, id, teache
   return html;
 }
 
-// This template contructs a table row for the history of requests table.
 function templateHistory(teacherName, type, courseType, oldDate, newDate, id, teacherID, oldRoom, numberOfStudents, status, notes, newRoom, comments){
   var html = '<tr class="tr-shadow" request-entry>' +
   '<td>'+teacherName+'</td>'+
@@ -59,14 +56,13 @@ function templateHistory(teacherName, type, courseType, oldDate, newDate, id, te
 
 $(document).ready(function() {
 
-  // Retrieve all the requests.
   $.ajax({
     url: '/horus/requests',
     type: 'GET',
     dataType: 'json',
   })
   .done(function(data) {
-  //  console.log(data);
+    console.log(data);
     for(i = 0; i < data.length; i++) {
       var teacherName = data[i].teacherName;
       var type = data[i].type;
@@ -85,7 +81,6 @@ $(document).ready(function() {
       var requestTableBody = $('#request-table').find('tbody');
       var historyTableBody = $('#history-table').find('tbody');
 
-      // Check if a request should be shown on the dashboard or on the history popup.
       if(status == 'pending'){
         var html = templateNew(teacherName, type, courseType, oldDate, newDate, id, teacherID, oldRoom, numberOfStudents, status, notes);
         requestTableBody.append(html);
@@ -99,7 +94,6 @@ $(document).ready(function() {
       var thisID;
       var userID = Cookies.getJSON('relevantData').teacherID;
 
-      // Button trigger which will expand the details of a request.
       $('.show-info').off().on('click', function(event){
         event.stopPropagation();
         var closest_tr = $(this).closest('tr');
@@ -107,7 +101,6 @@ $(document).ready(function() {
         hidden_tr.slideToggle('fast');
       });
 
-      // Button trigger which will open a popup for accepting a request.
       $('.accept-button').on('click', function(event){
         thisID = $(this).closest('div').siblings('ul').find('li').first().text().substr(12);
         var thisType;
@@ -117,82 +110,31 @@ $(document).ready(function() {
             break;
           }
         }
-
-        // Display different modals if the request requires a new room or not.
         if(thisType == 'reschedule'){
           $('#accept-modal').modal('toggle');
         } else if(thisType == 'cancel'){
           $('#accept2-modal').modal('toggle');
         }
+        //console.log(thisID);
       });
 
-      // Button trigger for declining a request.
       $('.decline-button').on('click', function(event){
         $('#cancel-modal').modal('toggle');
         thisID = $(this).closest('div').siblings('ul').find('li').first().text().substr(12, 2);
+        //console.log(thisID);
       });
 
-      // Button trigger for sending a PUT request to the server, informing it that the request that requires a new room was accepted.
       $('.accept-request').off().on('click', function(event){
         event.stopPropagation();
         var newRoom = $(this).closest('.modal-content').find('.new-room option:selected').val();
         var otherDetails = $(this).closest('.modal-content').find('.other-details').val();
         var userID = Cookies.getJSON('relevantData').teacherID;
-        var teacherID;
-        for(i = 0; i < data.length; i++) {
-          if(data[i].id == thisID){
-            teacherID = data[i].teacherID;
-          }
-        }
-
-      //  console.log(newRoom);
+        console.log(newRoom);
         var changeStatus = JSON.stringify({
           'status' : 'accepted',
           'id' : thisID,
           'comments' : otherDetails,
           'newRoom' : newRoom,
-          'userID' : userID,
-          'teacherID' : teacherID
-        });
-            $.ajax({
-              url: '/horus/requests/statusChange',
-              type: 'PUT',
-              dataType: 'json',
-              data: changeStatus,
-              headers: {
-                'Accept': 'application/json',
-                'Content-Type' : 'application/json'
-              },
-              complete: function(result){
-                if(result.status == 200) {
-                //  console.log("Status changed!");
-                  location.reload();
-                } else {
-                  console.log(result.status + " " + result.errorMessage);
-                }
-              }
-            });
-
-      });
-
-      // Button trigger for sending a PUT request to the server, informing it that the request that does not require a new room was accepted.
-      $('.accept2-request').off().on('click', function(event){
-        event.stopPropagation();
-        var otherDetails = $(this).closest('.modal-content').find('.other-details').val();
-        var userID = Cookies.getJSON('relevantData').teacherID;
-        var teacherID;
-        for(i = 0; i < data.length; i++) {
-          if(data[i].id == thisID){
-            teacherID = data[i].teacherID;
-          }
-        }
-      //  console.log(teacherID);
-        var changeStatus = JSON.stringify({
-          'status' : 'accepted',
-          'id' : thisID,
-          'comments' : otherDetails,
-          'newRoom' : 'Not specified!',
-          'teacherID' : teacherID,
           'userID' : userID
         });
             $.ajax({
@@ -206,7 +148,7 @@ $(document).ready(function() {
               },
               complete: function(result){
                 if(result.status == 200) {
-                  //console.log("Status changed!");
+                  console.log("Status changed!");
                   location.reload();
                 } else {
                   console.log(result.status + " " + result.errorMessage);
@@ -216,24 +158,46 @@ $(document).ready(function() {
 
       });
 
-      // Button trigger for sending a PUT request to the server, informing it that the request was not accepted.
+      $('.accept2-request').off().on('click', function(event){
+        event.stopPropagation();
+        var otherDetails = $(this).closest('.modal-content').find('.other-details').val();
+        console.log(newRoom);
+        var changeStatus = JSON.stringify({
+          'status' : 'accepted',
+          'id' : thisID,
+          'comments' : otherDetails,
+          'newRoom' : 'Not specified!'
+        });
+            $.ajax({
+              url: '/horus/requests/statusChange',
+              type: 'PUT',
+              dataType: 'json',
+              data: changeStatus,
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type' : 'application/json'
+              },
+              complete: function(result){
+                if(result.status == 200) {
+                  console.log("Status changed!");
+                  location.reload();
+                } else {
+                  console.log(result.status + " " + result.errorMessage);
+                }
+              }
+            });
+
+      });
 
       $('.cancel-request').off().on('click', function(event){
         event.stopPropagation();
         var otherDetails = $(this).closest('.modal-content').find('.other-details').val();
-        var teacherID;
-        for(i = 0; i < data.length; i++) {
-          if(data[i].id == thisID){
-            teacherID = data[i].teacherID;
-          }
-        }
         var changeStatus = JSON.stringify({
           'status' : 'cancelled',
           'id' : thisID,
           'comments' : otherDetails,
           'newRoom' : 'Not specified!',
-          'userID' : userID,
-          'teacherID' : teacherID
+          'userID' : userID
         });
 
             $.ajax({
@@ -247,7 +211,7 @@ $(document).ready(function() {
               },
               complete: function(result){
                 if(result.status == 200) {
-                //  console.log("accepted ok!");
+                  console.log("accepted ok!");
                   location.reload();
                 } else {
                   console.log(result.status + " " + result.errorMessage);
@@ -256,12 +220,14 @@ $(document).ready(function() {
             });
       });
 
+    //for(i = totalData; i >= totalData - 5; i--);
+    //console.log('This is the value: ' + $("#old-room").val());
   })
   .fail(function(result) {
     console.log("error did not get requests");
     console.log(result.status + " " + result.errorMessage);
   })
   .always(function() {
-    //console.log("complete");
+    console.log("complete");
   });
 });
